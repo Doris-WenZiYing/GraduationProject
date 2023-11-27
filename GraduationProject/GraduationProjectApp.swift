d717a9e2422fcf04b73f6fe6d95305ad15b1d38b
Doris <doris070714@gmail.com>
2023-11-27 15:59:48 +0800

    Initial Commit

---
+++ GraduationProject/GraduationProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraduationProjectApp.swift
+//  GraduationProject
+//
+//  Created by Doris Wen on 2023/11/27.
+//
+
+import SwiftUI
+
+@main
+struct GraduationProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
